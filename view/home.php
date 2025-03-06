29d1635053897724f9f4498acbc733bbcf1a023d
BlazeDashX <refat00021@gmail.com>
2025-03-06 18:52:14 +0600

    Initial Commit

---
+++ view/home.php
@@ -0,0 +1,47 @@
+<html>
+    <head>
+        <title> Home </title>
+</head>
+<style>
+    table, th, td {
+  border: 1px solid black;
+}
+</style>
+<body>
+    <h1>
+    Hi
+    </h1>
+    <h2 style="color:powderblue;">This is a heading</h2>
+
+
+
+    <table>
+  <tr>
+    <th>Logo 1</th>
+    <th>Logo 2</th>
+    <th>Logo 3</th>
+  </tr>
+  <tr>
+    <td>
+        <img src="../img/abc.jpg" width="400" height="300">
+    </td>
+    <td>
+    <img src="../img/abc2.jpg" width="400" height="300">
+    </td>
+    <td>
+    <img src="../img/abc3.jpg" width="400" height="300">
+    </td>
+  </tr>
+  <tr>
+    <td>Price : 40 Tk</td>
+    <td>Price : 100 Tk</td>
+    <td>Price : 80 Tk</td>
+  </tr>
+  <tr>
+    <td>Stock available</td>
+    <td>No Stock</td>
+    <td>uPCOMING</td>
+  </tr>
+</table>
+</body>
+</html>
